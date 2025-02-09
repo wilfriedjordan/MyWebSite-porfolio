862680066ef8f91f22a3db65254b290f82ca4e18
wilfriedtankwe <132952991+wilfriedtankwe@users.noreply.github.com>
2025-02-09 20:31:29 +0100

    08*02*2025*version2

--- src/app/shared/Project.tsx
+++ src/app/shared/Project.tsx
@@ -2,7 +2,7 @@
 
 import {useEffect}from 'react'
 import Image from 'next/image.js'
-
+import Link from 'next/link.js';
 export default function Project() {
   useEffect(() => {
     const ratio = 0.1;
@@ -43,78 +43,84 @@ export default function Project() {
       <section className="Projet bg-[#f7f9fb] w-full h-auto p-4  justify-center flex  ">
         <div className=" max-lg:flex max-lg:flex-wrap grid grid-cols-3 justify-center gap-3 ">
           <div className="w-[300px] max-ssm:w-full h-auto rounded-lg overflow-hidden revelationProject-2">
-            <div className="h-[200px]">
-              <Image
-                src={"/assets/4.png"}
-                alt=""
-                width={200}
-                height={200}
-                className="rounded-lg h-full hover:scale-[1.1] w-full"
-              ></Image>
-            </div>
-            <div className="text p-3  bg-primary">
-              <div className="text-lg text-orange-400  ">Miaam</div>
-              <p className="text-white text-sm opacity-60 ">
-                Miaam est une application web de commande de nouriture en ligne
-                facilitant ainsi la vie des utilisateur
-              </p>
-              <div className="flex justify-end items-center gap-3 flex-wrap text-orange-500 underline">
-                <span>Angular</span>
-                <span>tailwincss</span>
-                <span>Expressjs</span>
-                <span>MySQL</span>
-                <span>FireBase</span>
+            <Link href={"https://github.com/wilfriedjordan/TheAwekening.git"}>
+              <div className="h-[200px]">
+                <Image
+                  src={"/assets/4.png"}
+                  alt=""
+                  width={200}
+                  height={200}
+                  className="rounded-lg h-full hover:scale-[1.1] w-full"
+                ></Image>
               </div>
-            </div>
+              <div className="text p-3  bg-primary">
+                <div className="text-lg text-orange-400  ">Miaam</div>
+                <p className="text-white text-sm opacity-60 ">
+                  Miaam est une application web de commande de nouriture en
+                  ligne facilitant ainsi la vie des utilisateur
+                </p>
+                <div className="flex justify-end items-center gap-3 flex-wrap text-orange-500 underline">
+                  <span>Angular</span>
+                  <span>tailwincss</span>
+                  <span>Expressjs</span>
+                  <span>MySQL</span>
+                  <span>FireBase</span>
+                </div>
+              </div>
+            </Link>
           </div>
           <div className="w-[300px] max-ssm:w-full h-auto rounded-lg overflow-hidden revelationProject-3">
-            <div className="h-[200px]">
-              <Image
-                src={"/assets/2.png"}
-                alt=""
-                width={200}
-                height={200}
-                className="rounded-lg h-full hover:scale-[1.1] w-full"
-              ></Image>
-            </div>
-            <div className="text p-3  bg-primary">
-              <div className="text-lg text-orange-400  ">Miaam</div>
-              <p className="text-white text-sm opacity-60 ">
-                Miaam est une application web de commande de nouriture en ligne
-                facilitant ainsi la vie des utilisateur
-              </p>
-              <div className="flex justify-end items-center gap-3 flex-wrap text-orange-500 underline">
-                <span>Angular</span>
-                <span>tailwincss</span>
-                <span>Expressjs</span>
-                <span>MySQL</span>
-                <span>FireBase</span>
+            <Link href={"https://github.com/wilfriedjordan/TheAwekening.git"}>
+              <div className="h-[200px]">
+                <Image
+                  src={"/assets/2.png"}
+                  alt=""
+                  width={200}
+                  height={200}
+                  className="rounded-lg h-full hover:scale-[1.1] w-full"
+                ></Image>
               </div>
-            </div>
+              <div className="text p-3  bg-primary">
+                <div className="text-lg text-orange-400  ">TheAwekening</div>
+                <p className="text-white text-sm opacity-60 ">
+                  TheAwekening Est un blog qui vous permet de faire votre
+                  veiller sur differents sujets et Technologies
+                </p>
+                <div className="flex justify-end items-center gap-3 flex-wrap text-orange-500 underline">
+                  <span>Angular</span>
+                  <span>tailwincss</span>
+                  <span>Expressjs</span>
+                  <span>MySQL</span>
+                  <span>FireBase</span>
+                </div>
+              </div>
+            </Link>
           </div>
           <div className="w-[300px] max-ssm:w-full h-auto rounded-lg overflow-hidden revelationProject-4">
-            <div className="h-[200px] ">
-              <Image
-                src={"/assets/5.png"}
-                alt=""
-                width={200}
-                height={200}
-                className="rounded-lg h-full hover:scale-[1.1] w-full"
-              ></Image>
-            </div>
-            <div className="text p-3  bg-primary">
-              <div className="text-lg text-orange-400">ShopMaket</div>
-              <p className="text-white text-sm opacity-60 ">
-                ShopMaket est une solution pour trouvée facilement des produits
-                donc vous avez besoin proche de chez vous!
-              </p>
-              <div className="flex justify-end items-center gap-3 flex-wrap text-orange-500 underline">
-                <span>Nextjs</span>
-                <span>MySQL</span>
-                <span>FireBase</span>
-                <span>tailwincss</span>
+            <Link href={"https://github.com/wilfriedjordan/SolutionMarket.git"}>
+              <div className="h-[200px] ">
+                <Image
+                  src={"/assets/5.png"}
+                  alt=""
+                  width={200}
+                  height={200}
+                  className="rounded-lg h-full hover:scale-[1.1] w-full"
+                ></Image>
               </div>
-            </div>
+              <div className="text p-3  bg-primary">
+                <div className="text-lg text-orange-400">ShopMaket</div>
+                <p className="text-white text-sm opacity-60 ">
+                  ShopMaket est une solution pour trouvée facilement des
+                  produits donc vous avez besoin proche de chez vous!
+                </p>
+                <div className="flex justify-end items-center gap-3 flex-wrap text-orange-500 underline">
+                  <span>Nextjs</span>
+                  <span>MySQL</span>
+                  <span>FireBase</span>
+                  <span>tailwincss</span>
+                </div>
+              </div>
+            </Link>
           </div>
         </div>
       </section>
